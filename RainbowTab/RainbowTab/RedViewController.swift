7d0615531d45758008d2b7e3a6e36a0925f40e0f
dwayne lewis <lewis.a.dwayne@gmail.com>
2018-09-04 22:08:08 -0500

    RainbowTabs from Apple IOS Development book

---
+++ RainbowTab/RainbowTab/RedViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  RainbowTab
+//
+//  Created by Dwayne Lewis on 8/23/18.
+//  Copyright © 2018 Dwayne Lewis. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //“To assign a badge in code, set the badgeValue property to a non-nil string. You can access your view controller's UITabBarItem instance through its tabBarItem property.”
+        
+            
+             //tabBarItem.badgeValue = "!"
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
